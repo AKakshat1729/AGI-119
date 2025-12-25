cd953adaf090f504bffb54d09aec694703cef3be
nomaantalib <mohdnomaantalib@gmail.com>
2025-12-25 15:13:56 +0530

    feat: transform to responsive AI therapist chatbot with text/audio input

--- perception/nlu/nlu_live.py
+++ perception/nlu/nlu_live.py
@@ -5,6 +5,7 @@ nltk.download("punkt_tab", quiet=True)
 nltk.download("averaged_perceptron_tagger_eng", quiet=True)
 nltk.download("maxent_ne_chunker_tab", quiet=True)
 nltk.download("words", quiet=True)
+nltk.download("stopwords", quiet=True)
 
 def get_entities(text):
     tokens = nltk.word_tokenize(text)
